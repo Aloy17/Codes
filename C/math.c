0fcf199a42a6789865a2983da74260c6e83245c3
Ryane <ryaneb2004@gmail.com>
2024-02-14 20:40:35 +0530

    Added math functions

---
+++ C/math.c
@@ -0,0 +1,12 @@
+//Math functions
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a number ");
+    scanf("%d",a);
+    printf("Square root of the number is %d\n",sqrt(a));
+    printf("That number raised to the power of 2 is %d\n",pow(a,2));
+    printf("Absolute value of the given number is %d\n",abs(a));
+
+}
