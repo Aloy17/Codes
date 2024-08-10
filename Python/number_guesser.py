74df3f76103d9d334ef8965ca9a790aea893d6aa
Ryane <ryaneb2004@gmail.com>
2024-08-10 19:29:27 +0530

    Added my first Python project

--- Python/number_guesser.py
+++ Python/number_guesser.py
@@ -1,31 +1,29 @@
 import time
 import random
+print("Hello, this is a small program that'll try to guess your number")
+time.sleep(2)
+print("Please enter a range (Between which 2 numebrs will your original number be)")
+m = int(input("Range1:"))
+n = int(input("Range2:"))
+
+nom = int(input("Great now enter your number (don't worry I won't peak :-)"))
+
 while True:
- print("GREETINGS! I challenge you to pick a number and I shall guess it\n")
- m=int(input("Enter the first number: "))
- n=int(input("Enter the second number: "))
- 
- x=int(input("Now for the greatest part! enter your number and I shall guess it (don't worry I can't read the number)\n"))
- 
- print(f"Now I shall guess the number which is between {m} and {n} \n")
- time.sleep(1)
- print(".")
- time.sleep(1)
- print(".")
- time.sleep(1)
- print(".")
- time.sleep(1)
- print(".")
- time.sleep(1)
- print(".")
- 
- print("Is THIS your number? ",random.randrange(m,n+1))
- ansr=input().lower()
- 
- if ansr=="yes":
-     print("I knew it, I'm the best!")
-     break
- else:
-     ansr1=input("Awwwwww, can I try again? from the start?: ").lower()
-     if ansr1!="yes":
-         break
+    print("I'll now try to guess your number")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    num = random.randint(m,n)
+    print(f"Is THIS you number? {num} (yes or no)")
+    ansr = input()
+    if ansr.lower()=="yes":
+        print("Fantastic")
+        break
+    
+    else:
+        print("Awwwww i'll try again!")
+        
+        
